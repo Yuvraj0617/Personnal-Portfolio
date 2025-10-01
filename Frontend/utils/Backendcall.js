ed3c5b680f5b1a72089c1896c9c30cdc2d8d3ae7
Yubraj <yuvrajsapkota21@gmail.com>
2025-10-01 15:21:09 +0545

    change url for deploying

--- Frontend/utils/Backendcall.js
+++ Frontend/utils/Backendcall.js
@@ -1,3 +1,3 @@
-const base_url = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000"
-
+const base_url = import.meta.env.VITE_BASE_URL;
+console.log("Base URL:", base_url);
 export default base_url;
