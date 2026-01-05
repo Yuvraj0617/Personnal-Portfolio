92e1c8ee0830f18eb151bd0f5f464e97d090b0b3
Yubraj Sapkota <165939125+Yuvraj0617@users.noreply.github.com>
2026-01-06 00:30:22 +0530

    Update social media links in SocialMedia.jsx

--- Frontend/src/Components/Contact/SocialMedia.jsx
+++ Frontend/src/Components/Contact/SocialMedia.jsx
@@ -5,16 +5,16 @@ export default function SocialMedia() {
         <div className="text-center md:text-start">
             <p className="text-white text-xl font-bold">Connect with me:</p>
             <div className="flex space-x-4 mt-2">
-                <a href="#" className="text-white">
+                <a href="https://www.facebook.com/yubraj.sapkota.783302" className="text-white">
                     <FaFacebook size={28} className="hover:text-blue-600" />
                 </a>
-                <a href="#" className="text-white">
+                <a href="https://www.linkedin.com/in/yubraj-sapkota-a11288339/" className="text-white">
                     <FaLinkedin size={28} className="hover:text-blue-500" />
                 </a>
-                <a href="#" className="text-white">
+                <a href="https://www.instagram.com/yuvraaj_sapkota/" className="text-white">
                     <FaInstagram size={28} className="hover:text-pink-400" />
                 </a>
-                <a href="#" className="text-white">
+                <a href="https://github.com/Yuvraj0617" className="text-white">
                     <FaGithub size={28} className="hover:text-gray-400" />
                 </a>
             </div>
